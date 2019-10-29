44548d193d89ff6d2106a12cf9290be4a605f558
Natalia Nazaruk <nazarjazz@wp.pl>
2019-10-29 09:00:52 +0100

    Functionality to delete wines added

--- app/src/main/java/com/example/mywine/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/mywine/ui/main/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.mywine.ui.main
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -48,23 +50,23 @@ class MainActivity : AppCompatActivity() {
         recycler_view.adapter = adapter
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        menuInflater.inflate(R.menu.main_menu, menu)
-//        return true
-//    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
 
-//    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-//        return when (item?.itemId) {
-//            R.id.delete_all_notes -> {
-//                noteViewModel.deleteAllNotes()
-//                Toast.makeText(this, "All notes deleted!", Toast.LENGTH_SHORT).show()
-//                true
-//            }
-//            else -> {
-//                super.onOptionsItemSelected(item)
-//            }
-//        }
-//    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.delete_all_wines -> {
+                noteViewModel.deleteAllWines()
+                Toast.makeText(this, "All wines deleted!", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
