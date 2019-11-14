98f13787aa0d74b80f613528ff84b8ada5ce6e1f
Natalia Nazaruk <nazarjazz@wp.pl>
2019-11-14 20:04:02 +0100

    Added binding to views

--- app/src/main/java/pl/nataliana/mywine/adapter/WineAdapter.kt
+++ app/src/main/java/pl/nataliana/mywine/adapter/WineAdapter.kt
@@ -5,7 +5,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.mywine.R
 import com.example.mywine.databinding.WineItemBinding
 import pl.nataliana.mywine.model.Wine
 
@@ -24,72 +23,11 @@ class WineAdapter : ListAdapter<Wine, WineAdapter.ViewHolder>(WineDiffCallback()
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: Wine) {
-            binding.textViewName.text = item.name
-            setupColorHolder(item)
-            binding.textViewYear.text = item.year.toString()
-            setupRateHolder(item)
+            binding.wine = item
+            binding.executePendingBindings()
         }
-
-        private fun setupRateHolder(item: Wine) {
-            when (item.rate) {
-                5 -> {
-                    binding.grape1.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                    binding.grape2.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                    binding.grape3.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                    binding.grape4.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                    binding.grape5.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                }
-                4 -> {
-                    binding.grape1.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                    binding.grape2.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                    binding.grape3.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                    binding.grape4.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                    binding.grape5.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                }
-                3 -> {
-                    binding.grape1.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                    binding.grape2.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                    binding.grape3.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                    binding.grape4.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                    binding.grape5.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                }
-                2 -> {
-                    binding.grape1.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                    binding.grape2.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                    binding.grape3.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                    binding.grape4.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                    binding.grape5.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                }
-                1 -> {
-                    binding.grape1.setImageResource(R.drawable.ic_grape_rate_icon_checked)
-                    binding.grape2.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                    binding.grape3.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                    binding.grape4.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                    binding.grape5.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                }
-                0 -> {
-                    binding.grape1.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                    binding.grape2.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                    binding.grape3.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                    binding.grape4.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                    binding.grape5.setImageResource(R.drawable.ic_grape_rate_icon_unchecked)
-                }
-            }
-        }
-
-        private fun setupColorHolder(item: Wine) {
-            when (item.color) {
-                RED -> binding.wineImage.setImageResource(R.drawable.red_wine_glass)
-                WHITE -> binding.wineImage.setImageResource(R.drawable.white_wine_glass)
-                PINK -> binding.wineImage.setImageResource(R.drawable.pink_wine_glass)
-            }
-        }
-
 
         companion object {
-            private const val RED = "red"
-            private const val PINK = "pink"
-            private const val WHITE = "white"
 
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
