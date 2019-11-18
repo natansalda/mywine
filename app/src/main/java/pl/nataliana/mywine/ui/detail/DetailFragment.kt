5f75267b46b13c45d9ff7453c3bcab5aa3122bc3
Natalia Nazaruk <nazarjazz@wp.pl>
2019-11-18 11:28:53 +0100

    Update to DetailFragment

--- app/src/main/java/pl/nataliana/mywine/ui/detail/DetailFragment.kt
+++ app/src/main/java/pl/nataliana/mywine/ui/detail/DetailFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.mywine.R
 import com.example.mywine.databinding.FragmentWineDetailBinding
 import pl.nataliana.mywine.database.WineDatabase
@@ -25,7 +27,7 @@ class DetailFragment : Fragment() {
         )
 
         val application = requireNotNull(this.activity).application
-//        val arguments = DetailFragmentArgs.fromBundle(arguments!!)
+        val arguments = DetailFragmentArgs.fromBundle(arguments!!)
 
         val dataSource = WineDatabase.getInstance(application).wineDatabaseDao
         val viewModelFactory = WinesListViewModelFactory(dataSource, application)
@@ -35,20 +37,14 @@ class DetailFragment : Fragment() {
                 this, viewModelFactory
             ).get(WinesListViewModel::class.java)
 
-//        binding.winesListViewModel = wineListViewModel
+        binding.winesListViewModel = wineListViewModel
         binding.lifecycleOwner = this
 
-        // Add an Observer to the state variable for Navigating when a Quality icon is tapped.
-//        wineListViewModel.navigateToSleepTracker.observe(this, Observer {
-//            if (it == true) { // Observed state is true.
-//                this.findNavController().navigate(
-//                    SleepDetailFragmentDirections.actionSleepDetailFragmentToSleepTrackerFragment()
-//                )
-//                // Reset state to make sure we only navigate once, even if the device
-//                // has a configuration change.
-//                wineListViewModel.doneNavigating()
-//            }
-//        })
+        wineListViewModel.getAllWines().observe(this, Observer {
+            this.findNavController().navigate(
+                DetailFragmentDirections.actionDetailFragmentToMainFragment()
+            )
+        })
 
         return binding.root
     }
