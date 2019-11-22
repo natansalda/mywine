dc7ef4916e9b1b2c2e763d74074d99ccdc20a22f
Natalia Nazaruk <nazarjazz@wp.pl>
2019-11-22 21:54:08 +0100

    GetWineDetailAsyncTask added

--- app/src/main/java/pl/nataliana/mywine/WineRepository.kt
+++ app/src/main/java/pl/nataliana/mywine/WineRepository.kt
@@ -2,8 +2,8 @@ package pl.nataliana.mywine
 
 import android.os.AsyncTask
 import androidx.lifecycle.LiveData
-import pl.nataliana.mywine.model.Wine
 import pl.nataliana.mywine.database.WineDatabaseDao
+import pl.nataliana.mywine.model.Wine
 
 class WineRepository(private val wineDatabaseDao: WineDatabaseDao) {
 
@@ -25,6 +25,13 @@ class WineRepository(private val wineDatabaseDao: WineDatabaseDao) {
         return allWines
     }
 
+    fun getWineDetails(id: Long) {
+        GetWineDetailAsyncTask(
+            wineDatabaseDao
+            , id
+        ).execute()
+    }
+
     private class InsertWineAsyncTask(val wineDatabaseDao: WineDatabaseDao) : AsyncTask<Wine, Unit, Unit>() {
 
         override fun doInBackground(vararg wine: Wine?) {
@@ -36,6 +43,14 @@ class WineRepository(private val wineDatabaseDao: WineDatabaseDao) {
 
         override fun doInBackground(vararg p0: Unit?) {
             wineDatabaseDao.deleteAllWines()
+        }
+    }
+
+    private class GetWineDetailAsyncTask(val wineDatabaseDao: WineDatabaseDao, val id: Long) :
+        AsyncTask<Unit, Unit, Unit>() {
+
+        override fun doInBackground(vararg p0: Unit?) {
+            wineDatabaseDao.getWineDetails(id)
         }
     }
 }
--- app/src/main/java/pl/nataliana/mywine/ui/detail/DetailFragment.kt
+++ app/src/main/java/pl/nataliana/mywine/ui/detail/DetailFragment.kt
@@ -11,14 +11,17 @@ import androidx.navigation.fragment.navArgs
 import com.example.mywine.R
 import com.example.mywine.databinding.FragmentWineDetailBinding
 import org.koin.android.ext.android.inject
+import pl.nataliana.mywine.WineRepository
 import pl.nataliana.mywine.database.WineDatabase
+import pl.nataliana.mywine.database.WineDatabaseDao
 import pl.nataliana.mywine.model.WinesListViewModel
 import pl.nataliana.mywine.model.WinesListViewModelFactory
 
 class DetailFragment : Fragment() {
 
-    private val winesListViewModel: WinesListViewModel by inject()
     private val args: DetailFragmentArgs by navArgs()
+    private val wineDatabaseDao: WineDatabaseDao by inject()
+    private val wineRepository = WineRepository(wineDatabaseDao)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,8 +30,6 @@ class DetailFragment : Fragment() {
 
         val application = requireNotNull(this.activity).application
         val id = args.id
-
-        winesListViewModel.getWineDetail(id)
 
         val dataSource = WineDatabase.getInstance(application).wineDatabaseDao
         val viewModelFactory = WinesListViewModelFactory(dataSource, application)
@@ -46,6 +47,8 @@ class DetailFragment : Fragment() {
 
         binding.winesListViewModel = wineListViewModel
         binding.lifecycleOwner = this
+
+        wineListViewModel.getWineDetail(id)
 
         return binding.root
     }
