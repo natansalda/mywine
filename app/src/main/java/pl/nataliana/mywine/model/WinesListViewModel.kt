faa201adb87233871d390b33edc76ec5b8dfacce
Natalia Nazaruk <nazarjazz@wp.pl>
2019-11-25 13:42:56 +0100

    Cleaning in WinesListViewModel

--- app/src/main/java/pl/nataliana/mywine/model/WinesListViewModel.kt
+++ app/src/main/java/pl/nataliana/mywine/model/WinesListViewModel.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Job
 import pl.nataliana.mywine.database.WineDatabaseDao
 
 class WinesListViewModel(
@@ -13,9 +13,7 @@ class WinesListViewModel(
 ) : AndroidViewModel(application) {
 
     private var viewModelJob = Job()
-    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
-    private var particularWine = MutableLiveData<Wine?>()
     private var allWines = database.getAllWines()
 
     private val _navigateToWineDetail = MutableLiveData<Wine>()
@@ -38,29 +36,8 @@ class WinesListViewModel(
         return allWines
     }
 
-    fun initializeThisWine(id: Long) {
-        uiScope.launch {
-            particularWine.value = getWineFromDatabase(id)
-        }
-    }
-
-    private suspend fun getWineFromDatabase(id: Long): Wine? {
-        return withContext(Dispatchers.IO) {
-            val wine = id.let { database.getWineDetails(it) }
-            wine
-        }
-    }
-
     fun onWineDetailNavigated() {
         _navigateToWineDetail.value = null
-    }
-
-    fun onAddWineNavigated() {
-        _navigateToAddWine.value = null
-    }
-
-    fun onMainFragmentNavigated() {
-        _navigateToMainFragment.value = null
     }
 
     override fun onCleared() {
