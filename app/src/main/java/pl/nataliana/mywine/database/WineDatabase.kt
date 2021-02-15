a3aaaed08008e9d209ad2e19a8f05cc9a62b831e
Natalia Nazaruk <nazarjazz@wp.pl>
2021-02-15 16:30:37 +0100

    Introduction of database migration

--- app/src/main/java/pl/nataliana/mywine/database/WineDatabase.kt
+++ app/src/main/java/pl/nataliana/mywine/database/WineDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import pl.nataliana.mywine.model.Wine
 
 @Database(entities = [Wine::class], version = 10, exportSchema = false)
@@ -15,6 +17,13 @@ abstract class WineDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: WineDatabase? = null
 
+        private val MIGRATION_9_10 = object : Migration(9, 10) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+//                database.execSQL("CREATE TABLE `Fruit` (`id` INTEGER, `name` TEXT, " +
+//                        "PRIMARY KEY(`id`))")
+            }
+        }
+
         fun getInstance(context: Context): WineDatabase {
             synchronized(this) {
                 var instance = INSTANCE
@@ -24,7 +33,7 @@ abstract class WineDatabase : RoomDatabase() {
                         WineDatabase::class.java,
                         "wines_database"
                     )
-                        .fallbackToDestructiveMigration()
+                        .addMigrations(MIGRATION_9_10)
                         .build()
                     INSTANCE = instance
                 }
